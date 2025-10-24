25e54bb8903d3f536310cd5bc9d9454750c1364a
Jharif Nelson Aparicio Casillas <jharif.aparicio.casillas@usfx.bo>
2025-10-24 07:53:01 -0400

    initial

---
+++ src/test/java/org/example/FactorialTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class FactorialTest {
+    @Test
+    void testFactorialDeCero() {
+        assertEquals(1, Factorial.calcular(0));
+    }
+    @Test
+    void testFactorialDeCinco() {
+        assertEquals(120, Factorial.calcular(5));
+    }
+    @Test
+    void testFactorialNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> Factorial.calcular(-1));
+    }
+}
